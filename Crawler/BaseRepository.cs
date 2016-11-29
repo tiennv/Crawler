09f38127009bea024a6a120c5df60ce061d5ebc8
tiennv.hp@gmail.com <1234567890abc>
2016-11-29 11:48:44 +0700

    update repository

---
+++ Crawler/BaseRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperExtensions;
+
+namespace Crawler
+{
+    public interface IBaseRepository<T> where T : ModalsBase, new()
+    {
+        T GetById(int id);
+        int Add(T t);
+        bool Update(T t);
+        bool Delete(int id);
+    }
+
+    public class BaseRepository<T> : IBaseRepository<T> where T : ModalsBase, new()
+    {       
+        public BaseRepository()
+        {
+     
+        }
+
+        private string _connectionString = "";
+
+        protected string ConnectionString
+        {
+            get
+            {
+                if (_connectionString == "" && ConfigurationManager.ConnectionStrings["Crawler"] != null)
+                {
+                    _connectionString = ConfigurationManager.ConnectionStrings["Crawler"].ConnectionString;
+                }
+                return _connectionString;
+            }
+        }
+
+        public int Add(T entity)
+        {
+            int insertedId = 0;
+            try
+            {
+
+                using (var sqlConnection = new SqlConnection(ConnectionString))
+                {
+                    sqlConnection.Open();
+                    insertedId = sqlConnection.Insert<T>(entity);                    
+                    sqlConnection.Dispose();
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            return insertedId;
+        }
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            return GetByIdFromDb(id);            
+        }
+
+        public IList<T> GetByIds(ICollection<int> ids)
+        {
+            return ids.Select(GetById).Where(t => t != null).ToList();
+        }
+
+        public bool Update(T entity)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+                if (sqlConnection.Update<T>(entity))
+                {                    
+                    return true;
+                }                
+                return false;
+            }
+        }
+
+        public T GetByIdFromDb(int id)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+
+                //var result = sqlConnection.Get<T>(id);
+
+                var result = sqlConnection.Query<T>($"select * from {typeof(T).Name} where id = {id}").FirstOrDefault();
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Crawler/Modals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public class ModalsBase
+    {
+        public int Id { get; set; }
+    }
+
+    public class Sites : ModalsBase
+    {
+        public string SiteUrl { get; set; }
+    }
+
+    public class Links : ModalsBase
+    {        
+        public string Link { get; set; }
+        public int SiteId { get; set; }
+        public string Avatar { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCrawler { get; set; }
+        public DateTime DateSynced { get; set; }
+        public int CountSynced { get; set; }
+        public int LinkType { get; set; }
+    }
+
+    public class Contents : ModalsBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Avatar { get; set; }
+        public string Size { get; set; }
+        public string Download { get; set; }
+        public string LinkDownload { get; set; }
+        public string AvatarBig { get; set; }
+        public string ImageContent { get; set; }
+        public DateTime DateSynced { get; set; }
+        public DateTime DateModify { get; set; }
+    }
+
+
+}
